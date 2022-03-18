8bacf9e0c09ac31b732664ca7f8fb3a17ba7ed24
Alexander Lindner <25225552+alexander-lindner@users.noreply.github.com>
2022-03-18 08:48:16 +0100

    * added updater

--- textool/app.go
+++ textool/app.go
@@ -2,9 +2,29 @@ package main
 
 import (
 	"github.com/alexander-lindner/latex/textool/commands"
+	"github.com/blang/semver"
+	"github.com/rhysd/go-github-selfupdate/selfupdate"
 	log "github.com/sirupsen/logrus"
 )
 
+const version = "2.1.3"
+
+func doSelfUpdate() {
+	v := semver.MustParse(version)
+	latest, err := selfupdate.UpdateSelf(v, "alexander-lindner/latex")
+	if err != nil {
+		log.Println("Binary update failed:", err)
+		return
+	}
+	if latest.Version.Equals(v) {
+		// latest version is the same as current version. It means current binary is up to date.
+		log.Println("Current binary is the latest version", version)
+		log.Println(latest)
+	} else {
+		log.Println("Successfully updated to version", latest.Version)
+		log.Println("Release note:\n", latest.ReleaseNotes)
+	}
+}
 func main() {
 	//log.SetFlags(0)
 	log.SetFormatter(&log.TextFormatter{
@@ -14,6 +34,8 @@ func main() {
 	})
 	log.SetLevel(log.InfoLevel)
 	log.SetReportCaller(false)
+
+	doSelfUpdate()
 
 	commands.EvalCli()
 }
